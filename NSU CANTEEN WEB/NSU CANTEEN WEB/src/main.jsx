ae6e2b63f3dbc249eeeaffca24c8a493c1f874ba
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2025-05-31 12:41:10 +0600

    Update main.jsx

--- NSU CANTEEN WEB/NSU CANTEEN WEB/src/main.jsx
+++ NSU CANTEEN WEB/NSU CANTEEN WEB/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StoreContextProvider>
