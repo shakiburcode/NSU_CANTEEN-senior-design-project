8a87c9620c663d0bf8dd51ccc7c58cf26e41b922
Shakibur Rahman <shakiburrahmanwork@gmail.com>
2026-02-10 00:48:39 +0600

    Add missing closing tags for JSX elements

--- NSU CANTEEN WEB/NSU CANTEEN WEB/src/main.jsx
+++ NSU CANTEEN WEB/NSU CANTEEN WEB/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StoreContextProvider>
